2dd36374a61fd4f530aa8897ef090e1feef9afca
vicsash <victorsash96@gmail.com>
2025-03-20 19:36:54 +0100

    Altering test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from PIL import Image
 import matplotlib.pyplot as plt
 
 # Load the trained model
-model_path = 'runs/train/exp_20250312_larg_250/model.pt'
+model_path = 'runs/train/exp_20250311_med_250/model.pt'
 model = YOLO(model_path)
 
 # Load the test image
